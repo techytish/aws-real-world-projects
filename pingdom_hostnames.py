32b28c919ac239f31103a6c286b4f08aa75f0594
TechyTish <techytish@outlook.com>
2023-03-04 21:59:47 +0000

    add: list of hosts for lambda to check

---
+++ pingdom_hostnames.py
@@ -0,0 +1,48 @@
+from domains import (
+  domain1, 
+  domain2, 
+  domain3, 
+  domain4,
+  domain5,
+  domain6,
+)
+from regions import aws_regions
+
+# The below is a list of all the hostnames in Pingdom
+# The comments next to each corresponds with the name of the check in Pingdom
+
+# List of Domains #1
+list_of_hosts1 = [
+  f"test1.{domain1}",  #Test 1 - Domain 1
+  f"test2.{domain1}",  #Test 2 - Domain 1
+]
+
+# List of Domains #2
+list_of_hosts2 = [
+  f"test1.{domain2}",  #Test 1 - Domain 2
+  f"test2.{domain2}",  #Test 2 - Domain 2
+]
+
+# List of Domains #3
+list_of_hosts3 = [
+  f"test1.{domain3}",  #Test 1 - Domain 3
+  f"test2.{domain3}",  #Test 2 - Domain 3
+]
+
+# List of Domains #4
+list_of_hosts4 = [
+  f"test1.{domain4}",  #Test 1 - Domain 4
+  f"test2.{domain4}",  #Test 2 - Domain 4
+]
+
+# List of Domains #5
+list_of_hosts5 = [
+  f"test1.{domain5}",  #Test 1 - Domain 5
+  f"test2.{domain5}",  #Test 2 - Domain 5
+]
+
+# List of Domains #6
+list_of_hosts6 = [
+  f"test1.{domain6}",  #Test 1 - Domain 6
+  f"test2.{domain6}",  #Test 2 - Domain 6
+]
