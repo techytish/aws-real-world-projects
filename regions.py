8e840aba0fd9656add89ba63befc40806c4ba908
TechyTish <techytish@outlook.com>
2023-03-04 22:03:49 +0000

    add: regions list for some of our domains to use

---
+++ regions.py
@@ -0,0 +1,41 @@
+aws_regions = [
+  "us-east-1",  # US East (N. Virginia)
+  "us-east-2",  # US East (Ohio)
+  "us-west-1",  # US West (N. California)
+  "us-west-2",  # US West (Oregon)
+  "af-south-1",  # Africa (Cape Town)
+  "ap-east-1",  # Asia Pacific (Hong Kong)
+  "ap-south-1",  # Asia Pacific (Mumbai)
+  "ap-northeast-3",  # Asia Pacific (Osaka-Local)
+  "ap-northeast-2",  # Asia Pacific (Seoul)
+  "ap-southeast-1",  # Asia Pacific (Singapore)
+  "ap-southeast-2",  # Asia Pacific (Sydney)
+  "ap-northeast-1",  # Asia Pacific (Tokyo)
+  "ca-central-1",  # Canada (Central)
+  "eu-central-1",  # Europe (Frankfurt)
+  "eu-west-1",  # Europe (Ireland)
+  "eu-west-2",  # Europe (London)
+  "eu-south-1",  # Europe (Milan)
+  "eu-west-3",  # Europe (Paris)
+  "eu-north-1",  # Europe (Stockholm)
+]
+
+# # NOT USED but maybe useful for you # #
+azure_regions = [
+  "australiaeast",  # Australia East
+  "australiasoutheast",  # Australia Southeast
+  "canadacentral",  # Canada Central
+  "canadaeast",  # Canada East
+  "centralus",  # US Central
+  "eastus",  # US East
+  "eastus2",  # US East 2
+  "northcentralus",  # US North Central
+  "northeurope",  # Europe North
+  "southcentralus",  # US South Central
+  "ukwest",  # UK West
+  "uksouth",  # UK South
+  "westcentralus",  # US West Central
+  "westeurope",  # Europe West
+  "westus",  # US West
+  "westus2",  # US West 2
+]
