5bdb9a1f27968122d48d9c6a247f09582d268a0e
TechyTish <techytish@outlook.com>
2023-03-05 14:08:19 +0000

    add: google domain

--- pingdom_hostnames.py
+++ pingdom_hostnames.py
@@ -11,37 +11,38 @@ from regions import aws_regions
 # The below is a list of all the hostnames in Pingdom
 # The comments next to each corresponds with the name of the check in Pingdom
 
-# List of Domains #1
+# List of Hostnames #1
 list_of_hosts1 = [
+  "www.google.co.uk",
   f"test1.{domain1}",  #Test 1 - Domain 1
   f"test2.{domain1}",  #Test 2 - Domain 1
 ]
 
-# List of Domains #2
+# List of Hostnames #2
 list_of_hosts2 = [
   f"test1.{domain2}",  #Test 1 - Domain 2
   f"test2.{domain2}",  #Test 2 - Domain 2
 ]
 
-# List of Domains #3
+# List of Hostnames #3
 list_of_hosts3 = [
   f"test1.{domain3}",  #Test 1 - Domain 3
   f"test2.{domain3}",  #Test 2 - Domain 3
 ]
 
-# List of Domains #4
+# List of Hostnames #4
 list_of_hosts4 = [
   f"test1.{domain4}",  #Test 1 - Domain 4
   f"test2.{domain4}",  #Test 2 - Domain 4
 ]
 
-# List of Domains #5
+# List of Hostnames #5
 list_of_hosts5 = [
   f"test1.{domain5}",  #Test 1 - Domain 5
   f"test2.{domain5}",  #Test 2 - Domain 5
 ]
 
-# List of Domains #6
+# List of Hostnames #6
 list_of_hosts6 = [
   f"test1.{domain6}",  #Test 1 - Domain 6
   f"test2.{domain6}",  #Test 2 - Domain 6
