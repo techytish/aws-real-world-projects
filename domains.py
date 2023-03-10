50a120d3bfe30a463d4e02d4c64867546691384f
TechyTish <techytish@outlook.com>
2023-03-10 17:27:23 +0000

    Update domains.py

--- domains.py
+++ domains.py
@@ -4,11 +4,7 @@ from regions import (
 )
 
 # AWS
-domain1 = f"api-gateway.{aws_regions}.amazonaws.com"  # api-gateway.us-east-1.amazonaws.com
-domain2 = f"prod.{aws_regions}.amazonaws.com"         # lambda.us-east-1.amazonaws.com
-domain3 = f"dev.{aws_regions}.amazonaws.com"          # s3.us-east-1.amazonaws.com
+aws_api_domain = f"execute-api.{'.'.join(aws_regions)}.amazonaws.com" # restapi domain
   
 # Azure
-domain4 = f"prod.{azure_regions}.azure.com"        # prod.ukwest.azure.com
-domain5 = f"dev.{azure_regions}.azure.com"         # dev.ukwest.azure.com
-domain6 = f"staging.{azure_regions}.azure.net"     # staging.ukwest.azure.net
+azure_regions = f"environment.{'.'.join(azure_regions)}.amazonaws.com"
